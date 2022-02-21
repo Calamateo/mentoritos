ef282c986b07b3831393ed2d9d49f7edd9244005
Regina Roji <reginaroji@gmail.com>
2022-02-21 10:26:35 -0600

    Added mail fix

--- src/modulos/contactUs.jsx
+++ src/modulos/contactUs.jsx
@@ -7,6 +7,7 @@ const ContactForm = () => {
         const [message, setmessage] = useState();
         const [lastName, setlastName] = useState();
         const handleSubmit= (e) => {
+
         sendMail(
             {
                 nane:name,
@@ -15,6 +16,12 @@ const ContactForm = () => {
                 email:emaill
             }
             );
+            setname('')
+            setlastName('')
+            setmessage('')
+            setmessage('')
+            setemaill('')
+        e.preventDefault();
         }
 
     return (
