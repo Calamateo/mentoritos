e4e03aad3c775d03c3d315de217b56797e0e4092
Jorge Mercado <mercadoruizjorgearturo@gmail.com>
2022-03-01 03:11:04 -0600

    nueva seccion mentorsMenu

---
+++ src/Redux/CardsMentores.js
@@ -0,0 +1,98 @@
+import { useEffect } from "react";
+
+export default function CardsMentores(props){
+
+    
+   
+
+function inputMentor(item,element,number=item.length) {
+   let conten="";
+   let star=""
+  
+  for (let i = 0; i < number; i++) {
+  
+  }
+    for (let index = 0; index < number ;index++) {
+        for (let i = 0; i < 5; i++) {
+            if (i<item[index].star) {
+                star+=`  <div class="col-1 ">
+                <img src="../STAR.svg" width="24px" alt="start" onclick=putStart(${index},${i+1})> 
+              </div>`;
+            } else {
+                star+=`<div class="col-1 ">
+                <img src="../STARg.svg" width="24px" alt="start" onclick=putStart(${index},${i+1})>
+              </div>`;
+            }
+        }
+        conten += `
+        <div name="item" className=" m-3 position-relative"  id= "mentorCard" style="width: 18rem;">
+        <a href="#mentorsCards" data-bs-toggle="modal" onclick=ConstructorModal(${index}) href="#">
+     <img src="${item[index].img}" className="  card-img-top"  data-bs-toggle="mentorsCards"  alt="..." style="border-radius: 30px;">
+     </a>
+    <div className="card-body position-absolute bottom-0  bg-dark bg-opacity-50 text-white col-12" style="width: 16.5rem; border-bottom-left-radius:30px; border-bottom-right-radius:30px;">
+          <h5 className="card-title">${item[index].name}</h5>
+        <div className="row justify-content-between">
+            <div className="col-4">
+            ${item[index].class}
+            </div>
+        <div className="col-7 text-end">
+              ${item[index].price} MNX/Hr
+    </div>
+</div>
+            <div className="row g-0 justify-content-around"> <!--contenedor estrellas-->
+                ${star}
+            </div>
+        </div>
+      </div>
+        `;
+        star=""
+    }
+    document.getElementById(`${element}`).innerHTML=conten
+   
+}
+ 
+
+ useEffect(() => {
+    inputMentor(props.mentor.items,"Cards-Mentores");
+  }, [])
+
+
+
+/*
+
+function ConstructorModal( id, array = props.mentor ){
+  let image = document.getElementById("imageProfile")
+  let contentMentor = document.getElementById("contentProfile")
+  image.src =`${array[id].img}`
+  contentMentor.innerHTML = `<h3>${array[id].name}</h3>
+  <h4>Clase impartida: ${array[id].class}</h4>
+  <h4>Modalidad: ${array[id].modality}</h4>
+  <p>Tarifas accesibles a tan solo: ${array[id].price} Pesos /hora</p>
+  <button class="btn  me-3 rounded-pill btn-primary">solicitar clase</button> 
+  `;
+}
+*/
+
+  
+
+
+
+
+
+
+return(
+    <>
+    <h2>aqui va otra bendita seccion</h2>
+    <div class="container" id="Cards-Mentores">
+
+    </div>
+
+     
+
+
+
+    </>
+);
+
+
+}
--- src/modulos/MentorsMenu.jsx
+++ src/modulos/MentorsMenu.jsx
@@ -1,15 +1,135 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import CardsMentores from '../Redux/CardsMentores';
+import FiltroBusqueda from '../Redux/FiltroBusqueda';
 
 function MentorsMenu(){
+    
+
+    let info={
+        items:[
+            {
+                name:"Luis"
+                ,img:"https://images.pexels.com/photos/953703/pexels-photo-953703.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                ,star:5
+                ,class:"Música"
+                ,modality:"en linea"
+                ,price:150
+               
+            },
+            {
+                name:"Juan"
+                ,img:"https://images.pexels.com/photos/4307849/pexels-photo-4307849.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:5
+                ,class:"Pintura Óleo"
+                ,modality:"en linea"
+                ,price:200
+               
+            },
+            {
+                name:"Mon"
+                ,img:"https://images.pexels.com/photos/5795034/pexels-photo-5795034.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:4
+                ,class:"Danza Contemporanea"
+                ,modality:"en linea"
+                ,price:350
+            
+            },
+            {
+                name:"Karen"
+                ,img:"https://images.pexels.com/photos/6094038/pexels-photo-6094038.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:5
+                ,class:"Inglés"
+                ,modality:"en linea"
+                ,price:350
+              
+            },
+            
+            {
+                name:"Brenda"
+                ,img:"https://images.pexels.com/photos/7307380/pexels-photo-7307380.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:2
+                ,class:"Programación"
+                ,modality:"en linea"
+                ,price:250
+               
+            },
+            {
+                name:"Pablo"
+                ,img:"https://images.pexels.com/photos/2033287/pexels-photo-2033287.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:3
+                ,class:"Electrónica"
+                ,modality:"en linea"
+                ,price:300
+                
+            },
+            {
+                name:"Pedro"
+                ,img:"https://images.pexels.com/photos/5131401/pexels-photo-5131401.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:4
+                ,class:"Acuarela"
+                ,modality:"en linea"
+                ,price:350
+               
+            },
+            {
+                name:"Mario"
+                ,img:"https://images.pexels.com/photos/4607198/pexels-photo-4607198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:2
+                ,class:"Matemáticas"
+                ,modality:"en linea"
+                ,price:50
+                
+            },
+            {
+                name:"Luis"
+                ,img:"https://images.pexels.com/photos/10887630/pexels-photo-10887630.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:1
+                ,class:"Electrónica"
+                ,modality:"en linea"
+                ,price:150
+               
+            },
+            {
+                name:"Mayrelin"
+                ,img:"https://images.pexels.com/photos/5794945/pexels-photo-5794945.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                ,star:5
+                ,class:"Electrónica"
+                ,modality:"en linea"
+                ,price:300
+        
+            }
+        ]
+    }
+     
+    const[mentor,setMentor] = useState(info);
 
 
-    return( <div className="container-xl mb-5 pb-3">
-    <h2 className="text-center">Somos <strong> el vínculo </strong> entre mentores y estudiantes para facilitar el
-        intercambio de conocimientos.</h2>
-</div>
+    return (
+      <>
+        <div class="container">
+          <div className="mt-4" id="main">
+            <section className="titulo">
+              <div className="text-center mt-3  pt-5">
+                <h2>
+                  Elige a tu <strong>mentorito</strong> favorito
+                </h2>
+                <p>
+                  Aquí podrás encontrar una amplia variedad de mentores listos
+                  para acompañarte en tu proceso de aprendizaje.
+                </p>
+              </div>
+            </section>
 
-)
+            <hr />
+            <FiltroBusqueda  mentor={mentor} setMentor={setMentor}/>
+            <hr />
+             <CardsMentores mentor={mentor}/> 
+          </div>
+        </div>
+      </>
+    );
 
     
 }
+
+export default MentorsMenu
