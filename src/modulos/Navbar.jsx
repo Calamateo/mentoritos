623e79b0f966cd5bc0ccd0a40e11764e7bcdf6ae
Regina Roji <reginaroji@gmail.com>
2022-03-03 03:25:31 -0600

    deleted search bar

--- src/modulos/Navbar.jsx
+++ src/modulos/Navbar.jsx
@@ -34,17 +34,6 @@ function Navbar(props) {
                                 <Link className="navbar-brand" to="/"><img src={meteoro} alt=""
                                     width="50" className="d-inline-block align-text-middle" />
                                     Mentoritos</Link>
-                                <div className="ms-lg-4">
-                                    <form className="d-flex">
-                                        <input className="form-control me-lg-2 form-input rounded-pill" type="search"
-                                            placeholder="Encontrar un mentor" aria-label="Search" />
-                                        <button className="btn btn-outline-success rounded-pill" type="submit">Buscar</button>
-                                    </form>
-                                </div>
-                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
                                 <div className="collapse navbar-collapse justify-content-lg-end justify-content-sm-start align-items-center"
                                     id="navbarNavAltMarkup">
                                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
@@ -84,17 +73,6 @@ function Navbar(props) {
                             <Link className="navbar-brand" to="/"><img src={meteoro} alt=""
                                 width="50" className="d-inline-block align-text-middle" />
                                 Mentoritos</Link>
-                            <div className="ms-lg-4">
-                                <form className="d-flex">
-                                    <input className="form-control me-lg-2 form-input rounded-pill" type="search"
-                                        placeholder="Encontrar un mentor" aria-label="Search" />
-                                    <button className="btn btn-outline-success rounded-pill" type="submit">Buscar</button>
-                                </form>
-                            </div>
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
                             <div className="collapse navbar-collapse justify-content-lg-end justify-content-sm-start"
                                 id="navbarNavAltMarkup">
                                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
